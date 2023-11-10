bea113d12247a3c3a28443e4f3a5266751c76b01
brayand227 <142859574+brayand227@users.noreply.github.com>
2023-11-09 22:07:23 -0500

    Add files via upload

---
+++ Seguimineto2/persona/src/main/java/biblioteca/com/Biblioteca.java
@@ -0,0 +1,124 @@
+package biblioteca.com;
+/*
+Biblioteca: Una clase que implemente la interfaz Préstamo y 
+gestione la colección de libros disponibles y los registros de préstamos 
+realizados por los clientes. Debe incluir métodos para prestar libros, 
+así como para mantener un registro de préstamos.
+*/
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+import javax.xml.crypto.Data;
+
+public class Biblioteca implements Prestamo {
+    private Collection<Libro> LibrosDisponibles;
+    private ArrayList<PrestamoLibro> librosPrestados;
+    private Collection<Libro> librosBiblioteca;
+    private ArrayList<Cliente> clientes;
+
+    public Biblioteca() {
+        LibrosDisponibles = new ArrayList<>();
+        librosPrestados = new ArrayList<>();
+        librosBiblioteca = new ArrayList<>();
+        clientes = new ArrayList<>();
+
+    }
+
+    /* Agregamos todos los libros que se han prestado */
+    public void prestarLibro(Libro libro, Cliente cliente) {
+
+        PrestamoLibro prestamoLibro = new PrestamoLibro(libro, cliente, null);
+
+        librosPrestados.add(prestamoLibro);
+
+    }
+
+    public void devolveLibro(Libro libro, Cliente cliente) {
+
+    }
+
+    /* funcion para buscar un libro por Autor */
+
+    public boolean BuscarLibroPorAutor(Libro libro) {
+        boolean centinela = false;
+        for (Libro libro2 : LibrosDisponibles) {
+            if (libro.getAutor().equals(libro2.getAutor())) {
+                centinela = true;
+                break;
+
+            }
+
+        }
+
+        return centinela;
+    }
+
+    /* buscar libro por titulo */
+    public boolean BuscarLibroPorTitulo(Libro libro) {
+        boolean centinela = false;
+        for (Libro libro2 : LibrosDisponibles) {
+            if (libro.getTitulo().equals(libro2.getTitulo())) {
+                centinela = true;
+                break;
+
+            }
+
+        }
+
+        return centinela;
+    }
+
+    /*
+     * generar informes de préstamos de un libro
+     * (devolver una colección de los préstamos que se han realizado a un libro
+     * recibido por parámetro).
+     */
+    public ArrayList<PrestamoLibro> PrestamosDeCadaLibro(Libro libro) {
+        ArrayList<PrestamoLibro> prestamo = new ArrayList<>();
+        for (PrestamoLibro i : librosPrestados) {
+            if (i.getLibro().equals(libro)) {
+
+                prestamo.add(i);
+
+            }
+
+        }
+
+        return prestamo;
+    }
+
+    /* registrar libro */
+    public void registrarLibro(Libro libro) {
+
+        librosBiblioteca.add(libro);
+
+    }
+
+    /* Registrar un cliente */
+    public void RegistrarCliente(Cliente cliente) {
+        clientes.add(cliente);
+
+    }
+
+    /*
+     * registrar devolución de un libro ( debe recibir de parámetro un libro y la
+     * fecha del prestamos,
+     * buscar el registro del préstamo que cumpla con esos criterios y asignar en
+     * fecha de devolución la fecha actual)
+     */
+    public void registrarDevolucion(Libro libro, Data fecha) {
+
+        for (PrestamoLibro i : librosPrestados) {
+
+            if (i.getLibro().equals(libro) && i.getFechaPrestamo().equals(fecha)) {
+
+            }
+            librosPrestados.remove(i);
+
+        }
+
+    }
+}
